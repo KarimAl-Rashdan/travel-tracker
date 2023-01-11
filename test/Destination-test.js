685858023364bedafe61013c0407187c2f999d9d
karimy <karim.alrashdan14@gmail.com>
2023-01-11 15:28:19 -0700

    Create Destination class and tests

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -5,7 +5,7 @@ describe("Destination", () => {
   let destination1;
 
   beforeEach(() => {
-    destination1 = new Destination({
+    destination1 = new Destination(
       {
         "id": 1,
         "destination": "Lima, Peru",
@@ -14,6 +14,21 @@ describe("Destination", () => {
         "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
         "alt": "overview of city buildings with a clear sky"
       }
-    })
+    );
+  });
+  it("Should be a function", () => {
+    expect(Destination).to.be.a("function");
+  });
+  it("Should instantiate a new Destination class", () => {
+    expect(destination1).to.be.an.instanceof(Destination);
+  });
+  it("Should hold destination data", () => {
+    console.log("here", destination1)
+    expect(destination1.id).to.equal(1);
+    expect(destination1.destination).to.equal("Lima, Peru");
+    expect(destination1.estimatedLodgingCostPerDay).to.equal(70);
+    expect(destination1.estimatedFlightCostPerPerson).to.equal(400);
+    expect(destination1.image).to.equal("https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80");
+    expect(destination1.alt).to.equal("overview of city buildings with a clear sky")
   })
-})
+});
