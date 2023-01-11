05f8ba3c862ed83e883db2d30cf247231a346659
karimy <karim.alrashdan14@gmail.com>
2023-01-11 14:36:23 -0700

    Create sample data for destinations

---
+++ test/Destination-test.js
@@ -0,0 +1,19 @@
+import { expect } from "chai";
+import Destination from "../src/Destination"
+
+describe("Destination", () => {
+  let destination1;
+
+  beforeEach(() => {
+    destination1 = new Destination({
+      {
+        "id": 1,
+        "destination": "Lima, Peru",
+        "estimatedLodgingCostPerDay": 70,
+        "estimatedFlightCostPerPerson": 400,
+        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        "alt": "overview of city buildings with a clear sky"
+      }
+    })
+  })
+})
