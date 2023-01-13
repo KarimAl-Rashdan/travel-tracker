edf3cb77dcd363b20490e9cd05d00d5604724f7f
karimy <karim.alrashdan14@gmail.com>
2023-01-13 14:20:27 -0700

    Create welcomeTraveler function

--- src/DestinationRepository.js
+++ src/DestinationRepository.js
@@ -3,11 +3,12 @@ class DestinationRepository {
   constructor(allDestinationData) {
     this.allDestinations = allDestinationData;
     this.currentDestinations;
+  }
   filterDestinationById(id) {
     const destination = this.allDestinations.filter(destination => destination.id === id);
     this.currentDestinations = new Destination(destination[0])
     return destination[0]
   }
-}
 
+}
 export default DestinationRepository;
--- src/scripts.js
+++ src/scripts.js
@@ -14,10 +14,63 @@ import DestinationRepository from "./DestinationRepository"
 import Trip from "./Trip"
 import TripRepository from "./TripRepository"
 
+let allTravelerData;
+let allDestinationData;
+let allTripData;
+let travelerRepository;
+let destinationRepository;
+let tripRepository;
+let currentTraveler;
+let currentTravelerID;
+
 const travelerAPI = "http://localhost:3001/api/v1/travelers"
 const destinationAPI = "http://localhost:3001/api/v1/destinations"
 const tripAPI = "http://localhost:3001/api/v1/trips"
 
+function getData() {
+  Promise.all([
+    getAPIData(travelerAPI),
+    getAPIData(destinationAPI),
+    getAPIData(tripAPI),
+  ])
+    .then((response) => {
+      allTravelerData = response[0].travelers;
+      allDestinationData = response[1].destinations;
+      allTripData = response[2].trips;
+      createClassInstance(allTravelerData, allDestinationData, allTripData)
+      getRandomTraveler(allTravelerData)
+    })
+    .catch((error) => console.log(error))
+}
 
+const welcomeSection = document.getElementById("welcome-traveler")
 
+window.addEventListener("load", getData)
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+function createClassInstance(dataSet1, dataSet2, dataSet3) {
+allTravelerData = dataSet1.map((traveler) => new Traveler(traveler));
+travelerRepository = new TravelerRepository(allTravelerData);
+allDestinationData = dataSet2.map((destination) => new Destination(destination));
+destinationRepository = new DestinationRepository(allDestinationData)
+allTripData = dataSet3.map((trip) => new Trip(trip))
+tripRepository = new TripRepository(allTripData)
+  console.log("travelers", travelerRepository)
+  console.log("destinations", destinationRepository)
+  console.log("trips", tripRepository)
+}
+
+function getRandomTraveler(travelerData) {
+  const randomID = Math.floor(Math.random() * travelerData.length)
+  currentTraveler = allTravelerData[randomID]
+  currentTravelerID = currentTraveler.id
+  console.log("currentTraveler", currentTraveler)
+  console.log("id", currentTravelerID)
+  welcomeTraveler()
+}
+
+function welcomeTraveler() {
+  console.log("here", currentTraveler.name)
+  welcomeSection.innerText = `Welcome ${currentTraveler.name}!`
+}
