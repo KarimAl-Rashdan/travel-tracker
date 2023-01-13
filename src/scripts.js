046de8cf0b85fd3da63134654acc21c8decf8d89
karimy <karim.alrashdan14@gmail.com>
2023-01-13 14:35:04 -0700

    Add console logs

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,15 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+//Import Data Section
 import './css/styles.css';
+import './images/turing-logo.png';
+import getAPIData from "./apiCalls";
+import Traveler from "./Traveler";
+import TravelerRepository from "./TravelerRepository";
+import Destination from "./Destination";
+import DestinationRepository from "./DestinationRepository";
+import Trip from "./Trip";
+import TripRepository from "./TripRepository";
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import getAPIData from "./apiCalls"
-import Traveler from "./Traveler"
-import TravelerRepository from "./TravelerRepository"
-import Destination from "./Destination"
-import DestinationRepository from "./DestinationRepository"
-import Trip from "./Trip"
-import TripRepository from "./TripRepository"
-
+//Global Variables Section
 let allTravelerData;
 let allDestinationData;
 let allTripData;
@@ -23,9 +19,9 @@ let tripRepository;
 let currentTraveler;
 let currentTravelerID;
 
-const travelerAPI = "http://localhost:3001/api/v1/travelers"
-const destinationAPI = "http://localhost:3001/api/v1/destinations"
-const tripAPI = "http://localhost:3001/api/v1/trips"
+const travelerAPI = "http://localhost:3001/api/v1/travelers";
+const destinationAPI = "http://localhost:3001/api/v1/destinations";
+const tripAPI = "http://localhost:3001/api/v1/trips";
 
 function getData() {
   Promise.all([
@@ -37,33 +33,35 @@ function getData() {
       allTravelerData = response[0].travelers;
       allDestinationData = response[1].destinations;
       allTripData = response[2].trips;
-      createClassInstance(allTravelerData, allDestinationData, allTripData)
-      getRandomTraveler(allTravelerData)
+      createClassInstance(allTravelerData, allDestinationData, allTripData);
+      getRandomTraveler(allTravelerData);
     })
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
 }
 
-const welcomeSection = document.getElementById("welcome-traveler")
+//Query Selector Section
+const welcomeSection = document.getElementById("welcome-traveler");
 
-window.addEventListener("load", getData)
+//Add Event Listener Section
+window.addEventListener("load", getData);
 
-
+//Functions
 function createClassInstance(dataSet1, dataSet2, dataSet3) {
-allTravelerData = dataSet1.map((traveler) => new Traveler(traveler));
-travelerRepository = new TravelerRepository(allTravelerData);
-allDestinationData = dataSet2.map((destination) => new Destination(destination));
-destinationRepository = new DestinationRepository(allDestinationData)
-allTripData = dataSet3.map((trip) => new Trip(trip))
-tripRepository = new TripRepository(allTripData)
+  allTravelerData = dataSet1.map((traveler) => new Traveler(traveler));
+  travelerRepository = new TravelerRepository(allTravelerData);
+  allDestinationData = dataSet2.map((destination) => new Destination(destination));
+  destinationRepository = new DestinationRepository(allDestinationData);
+  allTripData = dataSet3.map((trip) => new Trip(trip));
+  tripRepository = new TripRepository(allTripData);
 }
 
 function getRandomTraveler(travelerData) {
-  const randomID = Math.floor(Math.random() * travelerData.length)
-  currentTraveler = allTravelerData[randomID]
-  currentTravelerID = currentTraveler.id
-  welcomeTraveler()
+  const randomID = Math.floor(Math.random() * travelerData.length);
+  currentTraveler = travelerData[randomID];
+  currentTravelerID = currentTraveler.id;
+  welcomeTraveler();
 }
 
 function welcomeTraveler() {
-  welcomeSection.innerText = `Welcome ${currentTraveler.name}!`
+  welcomeSection.innerText = `Welcome ${currentTraveler.name}!`;
 }
