a83adbe7ccf7e48b658452aa5767ee1364ac5048
karimy <karim.alrashdan14@gmail.com>
2023-01-13 13:15:23 -0700

    Import all classes and getAPIData function

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,13 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import getAPIData from "./apiCalls"
+import Traveler from "./Traveler"
+import TravelerRepository from "./TravelerRepository"
+import Destination from "./Destination"
+import DestinationRepository from "./DestinationRepository"
+import Trip from "./Trip"
+import TripRepository from "./TripRepository"
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
